8950cd029d930d7904ea40a41c6bb062c66f37a1
Valera Vishnevskii <v.v.vishnevskiy@mbp16m1max.local>
2022-12-15 13:20:25 +0300

    Added generating function object against source code

--- extendedjsonschema/validator.py
+++ extendedjsonschema/validator.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from extendedjsonschema.errors import SchemaError
 from extendedjsonschema.schema import Schema
 from extendedjsonschema.schemas.draft_04.schema import Schema as SchemaDraft04
@@ -12,12 +14,18 @@ class Validator:
         schema = self._schema(schema_definition.get("$schema", "http://json-schema.org/draft-04/schema#"))
         program = schema.program(schema_definition)
         self.source_code = program.compile(False)
+        self.run = self._function()
 
     def _schema(self, dialect: str) -> Schema:
         try:
             return self._schemas[dialect]()
         except KeyError:
             raise SchemaError(["$schema"], f"Invalid dialect (a version of JSON Schema): {dialect}")
+
+    def _function(self) -> Callable:
+        state = {}
+        exec(self.source_code, state)
+        return state["program"]
 
     def __repr__(self):
         return self.source_code
